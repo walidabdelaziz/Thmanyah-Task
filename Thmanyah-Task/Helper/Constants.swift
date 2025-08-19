8bee89083779f4efedb54d04e90c5042a9f1dc51
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2025-08-20 01:42:05 +0300

    Add constants file

---
+++ Thmanyah-Task/Helper/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Thmanyah-Task
+//
+//  Created by Walid Ahmed on 20/08/2025.
+//
+
+struct Constants{
+    static var SHARED_IMAGE = "https://picsum.photos/600/600"
+    static var BASE_URL = "https://api-v2-b2sit6oh3a-uc.a.run.app/"
+    static var HOME_SECTIONS = BASE_URL + "home_sections"
+}
