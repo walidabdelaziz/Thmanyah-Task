05b13e459c7100e816b00416db28072dfa865aff
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2025-08-19 20:27:04 +0300

    Initial Commit

---
+++ Thmanyah-Task/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Thmanyah-Task
+//
+//  Created by Walid Ahmed on 19/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Thmanyah-Task/Thmanyah_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thmanyah_TaskApp.swift
+//  Thmanyah-Task
+//
+//  Created by Walid Ahmed on 19/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Thmanyah_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
