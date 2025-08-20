072d92e66bffb847a1d1135296323d64165a6af4
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2025-08-20 17:46:08 +0300

    Add section view component at home screen

--- Thmanyah-Task/PresentationLayer/Views/HomeScreen.swift
+++ Thmanyah-Task/PresentationLayer/Views/HomeScreen.swift
@@ -9,90 +9,50 @@ import SwiftUI
 
 struct HomeScreen: View {
     @State private var vm = HomeViewModel(useCase: HomeUseCaseImpl())
+    
     var body: some View {
-        ZStack{
+        ZStack {
             Color.appBlack.ignoresSafeArea()
             
-            ScrollView(.vertical, showsIndicators: false){
-                
-                LazyVStack{
-                    
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVStack(spacing: 16) {
                     headerView
-                    
                     
                     if let sections = vm.homeResponse.sections {
                         ForEach(Array(sections.enumerated()), id: \.offset) { index, section in
-                            VStack(alignment: .leading, spacing: 12) {
-                                
-                                // Section Title
-                                Text(section.name ?? "No Title")
-                                    .font(.ibmSemiBold(.title3))
-                                    .foregroundStyle(.appWhite)
-                                    .padding(.horizontal)
-                                
-                                // Horizontal ScrollView for items
-                                ScrollView(.horizontal, showsIndicators: false) {
-                                    LazyHStack(spacing: 12) {
-                                        ForEach(section.content ?? [], id: \.self) { content in
-                                     
-                                            
-                                            switch section.normalizedType {
-                                     
-                                            case "big_square":
-                                                BigSquareContentCell(content: content)
-                                                    .frame(width: UIScreen.main.bounds.width / 1.75)
-
-                                            case "square":
-                                                SquareContentCell(content: content)
-                                                    .frame(width: UIScreen.main.bounds.width / 3.2)
-
-                                            case "queue":
-                                                QueueContentCell(content: content)
-                                                    .frame(width: UIScreen.main.bounds.width * 0.75)
-
-                                            default:
-                                                EmptyView()
-                                            }
+                            SectionView(section: section)
+                                .onAppear {
+                                    if index == sections.count - 1 {
+                                        Task {
+                                            await vm.loadMore()
                                         }
                                     }
-                                    .padding(.horizontal)
                                 }
-                            }
-                            .padding(.vertical, 8)
-                            .onAppear {
-                                if index == sections.count - 1 {
-                                    Task {
-                                        await vm.loadMore()
-                                    }
-                                }
-                            }
                         }
                     }
-
+                    
                     if vm.isLoading {
                         ProgressView("Loading more...")
                             .frame(maxWidth: .infinity, alignment: .center)
+                            .padding()
                     }
                 }
-                
             }
         }
         .toolbarVisibility(.hidden, for: .navigationBar)
         .task {
-            Task { await vm.getSections() }
+            await vm.getSections()
         }
         .refreshable {
-            Task { await vm.refresh() }
+            await vm.refresh()
         }
-       
     }
+    
     private var headerView: some View {
         HomeHeaderView(
             title: "Welcome, Walid",
             notificationCount: 4,
-            onNotificationPressed: {
-                
-            }
+            onNotificationPressed: {}
         )
         .padding()
     }
