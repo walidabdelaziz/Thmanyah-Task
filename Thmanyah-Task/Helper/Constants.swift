fd195bd38b4c859959a0c2b8e1273de46c2f4289
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2025-08-21 01:52:02 +0300

    Add search sections api to constants

--- Thmanyah-Task/Helper/Constants.swift
+++ Thmanyah-Task/Helper/Constants.swift
@@ -11,6 +11,6 @@ struct Constants{
     static var SHARED_IMAGE = "https://picsum.photos/600/600"
     static var BASE_URL = "https://api-v2-b2sit6oh3a-uc.a.run.app/"
     static var HOME_SECTIONS = BASE_URL + "home_sections"
-    
+    static var SEARCH_SECTIONS = "https://mock.apidog.com/m1/735111-711675-default/search"
     static var CORNER_RADIUS: CGFloat = 10
 }
