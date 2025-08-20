4bcf2a7cab54e2e72a701f2663e10be13f98a7be
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2025-08-20 13:45:11 +0300

    Add square content section at home screen

--- Thmanyah-Task/PresentationLayer/Views/HomeScreen.swift
+++ Thmanyah-Task/PresentationLayer/Views/HomeScreen.swift
@@ -14,19 +14,62 @@ struct HomeScreen: View {
             Color.appBlack.ignoresSafeArea()
             
             ScrollView(.vertical, showsIndicators: false){
-                
                 LazyVStack{
-                    
                     headerView
                     
-              
-                
-                    
+                    if let sections = vm.homeResponse.sections {
+                        ForEach(Array(sections.enumerated()), id: \.offset) { index, section in
+                            VStack(alignment: .leading, spacing: 12) {
+                                
+                                // Section Title
+                                Text(section.name ?? "No Title")
+                                    .font(.ibmSemiBold(.title3))
+                                    .foregroundStyle(.appWhite)
+                                    .padding(.horizontal)
+                                
+                                // Horizontal ScrollView for items
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    LazyHStack(spacing: 12) {
+                                        ForEach(section.content ?? [], id: \.self) { content in
+                                            switch section.type {
+                                            case "square":
+                                                ContentSquareCell(content: content)
+                                    
+                                            default:
+                                                EmptyView()
+                                            }
+                                        }
+                                    }
+                                    .padding(.horizontal)
+                                }
+                            }
+                            .padding(.vertical, 8)
+                            .onAppear {
+                                if index == sections.count - 1 {
+                                    Task {
+                                        await vm.loadMore()
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if vm.isLoading {
+                        ProgressView("Loading more...")
+                            .frame(maxWidth: .infinity, alignment: .center)
+                    }
                 }
                 
             }
         }
         .toolbarVisibility(.hidden, for: .navigationBar)
+        .task {
+            Task { await vm.getSections() }
+        }
+        .refreshable {
+            Task { await vm.refresh() }
+        }
+       
     }
     private var headerView: some View {
         HomeHeaderView(
@@ -43,3 +86,4 @@ struct HomeScreen: View {
 #Preview {
     HomeScreen()
 }
+
